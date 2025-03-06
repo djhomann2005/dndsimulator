2f33dc5b07066f98453a2cf026cb667d072f12a9
DJ Homann <djhomann2005@gmail.com>
2025-03-05 19:09:51 -0600

    Improved dice_roll.js to add dice selection based on input

--- scripts/dice_rolls.js
+++ scripts/dice_rolls.js
@@ -15,9 +15,58 @@ function captureNumDiceInput() {
 }
 
 function renderDiceSelector(inputNumber) {
+    // variable to point to html element these new elements will be added to
+    const rollButton = document.getElementById("roll-button");
+    const diceContainer = document.getElementById("dice-container");
+    diceContainer.innerHTML = "";
 
+    for (let i = inputNumber; i > 0; i--) {
+        // create the necessary number of new divs
+        const newDiceDiv = document.createElement("div");
+        newDiceDiv.className = 'dice';
+        diceContainer.appendChild(newDiceDiv);
+        
+        // in each div, create a label "Dice Type"
+        const newLabel = document.createElement("label");
+        newLabel.htmlFor = `dieType${i}`;
+        newLabel.className = "label almendra-regular";
+        newLabel.innerHTML = "Die Type: ";
+        newDiceDiv.appendChild(newLabel);
+
+        // in each div, create a dropdown with dice type selections
+        const newDropDown = document.createElement('select');
+        newDropDown.id = `dieType${i}`;
+        newDropDown.className = "dieType";
+        newDropDown.required = true; // make each input required
+        newDiceDiv.appendChild(newDropDown);
+        
+        // Dice type options
+        const diceTypes = ["d4", "d6", "d8", "d10", "d12", "d20", "d100"];
+
+        // Populate dropdown with dice options
+        diceTypes.forEach(type => {
+            const option = document.createElement("option");
+            option.value = type;
+            option.textContent = type;
+            newDropDown.appendChild(option);
+        });
+    }
+    
+
+    // change display of 'Roll Dice' button so it shows
+    rollButton.style.display = "flex";
 }
 
 function numDiceSelect() {
     const input = captureNumDiceInput();
+    renderDiceSelector(input);
+}
+
+function rollDice() {
+    // keep a running total of dice rolls
+    // keep track of dice rolls individually
+    // get dieType (maximum) for each die 
+        // simulate the die roll
+            // record roll
+            // add roll to total
 }
